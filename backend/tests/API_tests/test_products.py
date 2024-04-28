a10c3a9f4c7165f60736aa03215cfc387548ebb3
EvanaPerveenEva <evana.perveen@welldev.io>
2024-04-28 16:23:22 +0600

    trying to add in-memory db for testing

--- backend/tests/API_tests/test_products.py
+++ backend/tests/API_tests/test_products.py
@@ -6,7 +6,6 @@ from app.models.sqlalchemy_models import Product
 from app.oauth2 import create_access_token, get_token_data
 
 
-
 @pytest.fixture
 def admin_token():
     user_data = {"user_id": 1, "role": "admin"}
@@ -93,16 +92,14 @@ def test_read_product(test_client, db_session, admin_token):
 def test_update_product(test_client, db_session, admin_token):
     product_id = 1
     existing_product = db_session.get(Product, product_id)
-    
-    # print("\n\n\n\n", existing_product.created_at)
 
     updated_product_data = {
-        "id": product_id, 
+        "id": product_id,
         "name": "Updated Test Product",
         "description": "Updated Test Description",
         "price": 150,
         "image_url": "updated_test.jpg",
-        "created_at": existing_product.created_at.isoformat(), 
+        "created_at": existing_product.created_at.isoformat(),
     }
 
     with patch("app.oauth2.jwt.decode") as mock_jwt_decode:
@@ -113,13 +110,41 @@ def test_update_product(test_client, db_session, admin_token):
             headers={"Authorization": f"Bearer {admin_token}"},
             json=updated_product_data,
         )
-        # print("\n\n\n\n\n", updated_product_data)
 
     assert response.status_code == status.HTTP_200_OK
 
-    updated_product = db_session.get(Product, product_id) 
+    updated_product = db_session.get(Product, product_id)
     assert updated_product.name == updated_product_data["name"]
     assert updated_product.description == updated_product_data["description"]
     assert updated_product.price == updated_product_data["price"]
     assert updated_product.image_url == updated_product_data["image_url"]
     assert updated_product.created_at == existing_product.created_at
+
+
+def test_delete_product_existing(test_client, db_session, admin_token):
+    product_id = 1
+
+    with patch("app.oauth2.jwt.decode") as mock_jwt_decode:
+        mock_jwt_decode.return_value = {"user_id": 1, "role": "admin"}
+
+        response = test_client.delete(
+            f"/api/products/{product_id}",
+            headers={"Authorization": f"Bearer {admin_token}"},
+        )
+
+    assert response.status_code == status.HTTP_200_OK
+
+    deleted_product = db_session.query(Product).filter(Product.id == product_id).first()
+    assert deleted_product is None
+
+
+def test_delete_product_non_existing(test_client, db_session, admin_token):
+    with patch("app.oauth2.jwt.decode") as mock_jwt_decode:
+        mock_jwt_decode.return_value = {"user_id": 1, "role": "admin"}
+
+        response = test_client.delete(
+            "/api/products/999", headers={"Authorization": f"Bearer {admin_token}"}
+        )
+
+    assert response.status_code == status.HTTP_404_NOT_FOUND
+    assert response.json()["detail"] == "Product not found"
